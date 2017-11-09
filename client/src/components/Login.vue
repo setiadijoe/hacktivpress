cb06a3d28753230809af99a9b5ec9fa4f09d58be
setiadijoe <setiadi_y56@yahoo.co.id>
2017-11-09 15:59:08 +0700

    Adding login component

---
+++ client/src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label class="col-form-label" for="inputDefault">UserName</label>
+      <input type="text" class="form-control" id="inputDefault">
+    </div>
+    <div class="form-group">
+      <label class="col-form-label" for="inputDefault">Password</label>
+      <input type="text" class="form-control" id="inputDefault">
+    </div>
+    <button type="submit" class="btn btn-primary">Sign In</button>
+    <small>Don't have any account? Click Sign Up</small>
+    <button type="submit" class="btn btn-secondary">Sign Up</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
